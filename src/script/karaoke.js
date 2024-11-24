8078d0141280e9efbf047018d10b934e4d8533a4
barbarauc3m <10049585@alumnos.uc3m.es>
2024-11-24 13:15:26 +0100

    pequeño fallito

--- src/script/karaoke.js
+++ src/script/karaoke.js
@@ -10,22 +10,7 @@ $(document).ready(function () {
         // Muestra el contenedor del vídeo e inserta el archivo de vídeo
         $(".contenedor-video").show();
         $("#karaoke-video").attr("src", videoURL);
-        
-    });
-});
-
-$(document).ready(function () {
-    $(".cancion").click(function () {
-        // Obtén la URL del vídeo del atributo data-video
-        const videoURL = $(this).data("video");
-
-        // Oculta las canciones y el personaje
-        $(".contenedor-canciones").hide();
-        $(".contenedor-personaje").hide();
-
-        // Muestra el contenedor del vídeo e inserta el archivo de vídeo
-        $(".contenedor-video").show();
-        $("#karaoke-video").attr("src", videoURL);
+        $(".flecha").show();
     });
 
     // Evento para la flecha que regresa a la vista de las canciones
