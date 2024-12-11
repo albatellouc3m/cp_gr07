dcee0ac04282e93b218d05a6a9864ad9ac20c035
albatellouc3m <100495755@alumnos.uc3m.es>
2024-12-11 15:04:50 +0100

    galeria mejorada

--- src/script/papaNoel.js
+++ src/script/papaNoel.js
@@ -72,7 +72,7 @@
                     { $zIndex: 2, width: 120, height: 140, top: '40%', left: '50%', $opacity: 0.5, marginTop: '-70px', marginLeft: '-60px' },
                     { $zIndex: 1, width: 100, height: 120, top: '50%', left: '50%', $opacity: 0.2, marginTop: '-60px', marginLeft: '-50px' }
                 ];
-            } else if (window.innerWidth <= 810) {
+            } else if (window.innerWidth <= 900) {
                 return [
                     { $zIndex: 1, width: 80, height: 100, top: 69, left: '0%', $opacity: 0.2 },
                     { $zIndex: 2, width: 100, height: 120, top: 59, left: '12%', $opacity: 0.5 },
@@ -84,13 +84,13 @@
                 ];
             } else {
                 return [
-                    { $zIndex: 1, width: 120, height: 150, top: 69, left: 134, $opacity: 0.2 },
-                    { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.5 },
-                    { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 0.7 },
-                    { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1 },
-                    { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
-                    { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.5 },
-                    { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 }
+                    { $zIndex: 1, width: 290, height: 290, top: 69, left: 134, $opacity: 0.2 },
+                    { $zIndex: 2, width: 300, height: 300, top: 59, left: 0, $opacity: 0.5 },
+                    { $zIndex: 3, width: 400, height: 400, top: 35, left: 110, $opacity: 0.7 },
+                    { $zIndex: 4, width: 500, height: 500, top: 0, left: 263, $opacity: 1 },
+                    { $zIndex: 3, width: 400, height: 400, top: 35, left: 470, $opacity: 0.7 },
+                    { $zIndex: 2, width: 300, height: 300, top: 59, left: 620, $opacity: 0.5 },
+                    { $zIndex: 1, width: 290, height: 290, top: 69, left: 500, $opacity: 0.2 }
                 ];
             }
         }
